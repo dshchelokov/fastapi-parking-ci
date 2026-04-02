562aa5f6a21f10cb751eae68af0a8eba3082c14d
root <root@DESKTOP-6KCD8J1.localdomain>
2026-04-02 22:02:03 +0300

    rechore code conftest.py with cconditions Github

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,21 @@
 import pytest
-
 from app import create_app, db
 
-
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def app():
-    app = create_app("config.TestConfig")
+    app = create_app()
     with app.app_context():
-        db.engine.execute("DROP TABLE IF EXISTS client_parking")
-        db.engine.execute("DROP TABLE IF EXISTS parking")
-        db.engine.execute("DROP TABLE IF EXISTS client")
         db.create_all()
         yield app
+        db.drop_all()
+
+@pytest.fixture(scope="function")
+def client(app):
+    return app.test_client()
+
+@pytest.fixture(scope="function")
+def db_session(app):
+    with app.app_context():
+        db.create_all()
+        yield db
         db.drop_all()
